d5b45e34eb9fc0527058ba1e4e7f2779ea6a5d98
kkeen699 <kkeen699@gmail.com>
2021-11-21 22:49:33 +0800

    update

--- opencv_training.py
+++ opencv_training.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('../training.jpg')
+img = cv2.imread('./image/opencv_training.jpg')
 
 ## problem 2 color detection: HSV + inRange() 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
